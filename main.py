563cb69a9dedf6fc2dcd3955a8ed08cc7085c9d8
LeYuwei <leyuwei126@126.com>
2018-11-26 16:23:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import seuhelper
 from seuhelper import *
 
 def main():
-	helper = seuhelper("220170889", "Leyuwei123")
+	helper = seuhelper("220170000", "123456")
 
 	# 单次操作：登录账户
 	# helper.command_login()
@@ -14,7 +14,7 @@ def main():
 	# helper.command_logout()
 
 	# 部署：10分钟检测一次登录状态，注销的话就自动重登
-	helper.keep_alive(10)
+	# helper.keep_alive(10)
 
 if __name__ == "__main__":
 	main()
